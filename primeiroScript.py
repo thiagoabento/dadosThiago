a5bbcc7ff130aee62d79ba1b3abe543d98d8e74b
= <=>
2025-05-23 15:23:56 -0300

    Atualizando arquivos

---
+++ primeiroScript.py
@@ -0,0 +1,7 @@
+num1 = int(input('Informe o primeiro número'))
+num2 = int(input('Informe o segundo número'))
+
+soma = num1 + num2
+
+#print('A soma entre', num1, 'e ',num2,' é igual a', soma)
+print('A soma entre {} e {} é igual a {}'.format(num1,num2,soma))
